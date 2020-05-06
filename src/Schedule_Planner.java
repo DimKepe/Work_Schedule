0fa184105b61a43903f02c11b5f28895adb67f7e
Dimitris Kepesidis <dimkepe@hotmail.com>
2020-05-06 19:31:09 +0300

    Update Schedule_Planner.java

--- src/Schedule_Planner.java
+++ src/Schedule_Planner.java
@@ -1,58 +1,79 @@
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Random;
 import java.lang.Math;
 
 public class Schedule_Planner {
 	
-	static ArrayList<Object> workers = new ArrayList<Object>(); 
-
-	@SuppressWarnings({ "null", "unchecked"})
+	static HashMap<Integer, String> workers = new HashMap<>();  
+	
+	public Schedule_Planner(HashMap<Integer, String> workers1) {
+		workers = workers1;
+	}
+	
+	@SuppressWarnings({ "unchecked"})
 	public static void main(String[] args) {
 		
-		for (int i = 0; i < 5; i++) {
-			workers.add(i+1);
-		}
-		ArrayList<Object> workclone = new ArrayList<Object>();
+		workers.put(0, "Dimitris");
+		workers.put(1, "Stella");
+		workers.put(2, "Vassilis");
+		workers.put(3, "Giwrgos");
+		workers.put(4, "Maria");
+		HashMap<Integer, String> workclone = workers;
 		
 		int num_days = 7,
-			num_shifts = 3;
+			num_shifts = 3,
+			max = workers.size();
 		int d, w = 0, s, i, j;
-		Object[][] working = new Object[num_shifts][num_days];
+		boolean firsttime = true;
+		Object[][] working = new Object[num_days][num_shifts];
 /*
-		for (i = 0; i < workers.length; i++) {
-			System.out.println(workers[i]);
-		}		
-*/		
-		
+		for (i = 0; i < workers.size(); i++) {
+			System.out.println(workers.get(i));
+		}	
+*/	
+	
+/*		
 		for (d = 0; d < num_days; d++) {
-			workclone = (ArrayList<Object>) workers.clone();
+			workclone = (HashMap<Integer, String>) workers.clone();
 			for (s = 0; s < num_shifts; s++) {
-				working[s][d] = workclone.get(w);
+				working[d][s] = workclone.get(w);
 				workclone.remove(w);
 				w++;
-				if(w == workclone.size()) w = 0;
+				if(w == workclone.size())  w = 0;
 			}
 		}
-	//	w = 0;
-		
-		for (d = 0; d < num_days; d++) {
-			workclone = (ArrayList<Object>) workers.clone();
-			for (s = 0; s < num_shifts; s++) {
-				for (i = 0; i < 2; i++) {
-					working[s][d] += "," + workclone.get(w);
-				//	workclone.remove(w);
-					w++;
-					if(w == workclone.size()) w = 0;
-				}
-		    }
-		}
-			
-		
+*/
+//		w = 0;
 	
-	    for ( i = 0; i < working.length; ++i) {
-	        for( j = 0; j < working[i].length; ++j) {
-	          System.out.print(working[i][j] + "	");
+		for (d = 0; d < num_days; d++) {
+			workclone = (HashMap<Integer,String>) workers.clone();
+			for (s = 0; s < num_shifts; s++) {
+				if (firsttime = true) {
+						working[d][s] = workclone.get(w);
+//						workclone.remove(w);
+						w++;
+						if(w == workclone.size())  w=0;
+						firsttime = false;	
+				} else {
+					for (i = 0; i < 2; i++) {
+						working[d][s] += "," + workclone.get(w);
+//						workclone.remove(w);
+						w++;
+						if(w == workclone.size())  w=0;
+					}
+				}
+
+			}
+		}
+		
+		
+		
+	    for ( d = 0; d < working.length; ++d) {
+	    	System.out.println("Day:" + (d + 1));
+	        for( s = 0; s < working[d].length; ++s) {
+	          System.out.printf("%-10s\t", working[d][s]);
 	        }
 	        System.out.println();
 	    } 
